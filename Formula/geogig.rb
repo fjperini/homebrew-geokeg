23ccec5a1c82c3897cb7258617949f77f6249a3b
Walker Hamilton <hey@walkerhamilton.com>
2017-05-11 14:22:48 -0500

    Update to 1.1.0
    ...

--- Formula/geogig.rb
+++ Formula/geogig.rb
@@ -1,8 +1,8 @@
 class Geogig < Formula
   desc "Tool to handle distributed versioning of geospatial data."
   homepage "http://geogig.org"
-  url "http://download.locationtech.org/geogig/geogig-1.0.zip"
-  sha256 "b034524860fda1f42d1cc6f72662540a0da450611027a96088a3cf137d7a4ff1"
+  url "http://download.locationtech.org/geogig/geogig-1.1.0.zip"
+  sha256 "6bc0eee8bb5a0d6bf27e5ce0cc7ba68b7e6e289731e837f1f51bb366b74ea7df"
 
   def install
     bin.install "bin/geogig"
