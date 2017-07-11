77683c82dc6327fea28c02424858b7dca340097c
Walker Hamilton <hey@walkerhamilton.com>
2017-07-11 09:22:50 -0500

    java dependency

--- Formula/geogig.rb
+++ Formula/geogig.rb
@@ -1,8 +1,11 @@
 class Geogig < Formula
-  desc "Tool to enable distributed versioning of geospatial data in a git-like context."
+  desc "Enable distributed versioning of geospatial data in a git-like context"
   homepage "http://geogig.org"
   url "http://download.locationtech.org/geogig/geogig-1.1.1.zip"
   sha256 "52d277bd9a18f071aff21fd4f58b1879fb2fad4dd3ddb589fc14050b9dd355bf"
+
+  option "with-java",              "Use Java, requires Java 8 from https://www.java.com/en/download/mac_download.jsp"
+  depends_on :java                 => ["1.8+"]
 
   def install
     bin.install "bin/geogig"
@@ -10,7 +13,7 @@ class Geogig < Formula
   end
 
   test do
-    system "#{bin}/geogig init"
-    system "#{bin}/geogig status"
+    system "#{bin}/geogig", "init"
+    system "#{bin}/geogig", "status"
   end
 end
