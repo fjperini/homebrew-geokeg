61b8968b85adaf383530951e95865824a30c4ab3
Walker Hamilton <hey@walkerhamilton.com>
2017-07-11 08:16:40 -0500

    Update geogig.rb

--- Formula/geogig.rb
+++ Formula/geogig.rb
@@ -1,5 +1,5 @@
 class Geogig < Formula
-  desc "Tool to handle distributed versioning of geospatial data."
+  desc "Tool to enable distributed versioning of geospatial data in a git-like context."
   homepage "http://geogig.org"
   url "http://download.locationtech.org/geogig/geogig-1.1.1.zip"
   sha256 "52d277bd9a18f071aff21fd4f58b1879fb2fad4dd3ddb589fc14050b9dd355bf"
