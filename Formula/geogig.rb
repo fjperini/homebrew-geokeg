1e7dd036fc47dc65a636d72446976e2536593155
Walker Hamilton <hey@walkerhamilton.com>
2017-07-13 13:31:02 -0500

    remove unnecessary spacing

--- Formula/geogig.rb
+++ Formula/geogig.rb
@@ -4,10 +4,10 @@ class Geogig < Formula
   url "http://download.locationtech.org/geogig/geogig-1.1.1.zip"
   sha256 "52d277bd9a18f071aff21fd4f58b1879fb2fad4dd3ddb589fc14050b9dd355bf"
 
-  depends_on :java                 => ["1.8+", :recommended]
+  depends_on :java => ["1.8+", :recommended]
 
   def install
-    args << "--without-java"             if build.without? "java"
+    args << "--without-java" if build.without? "java"
 
     bin.install "bin/geogig"
     prefix.install "lib"
